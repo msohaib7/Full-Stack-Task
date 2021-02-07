f7997bc04da575e5cecf85e170b33af84d37a7bb
msohaib7 <msohaib7@hotmail.com>
2021-02-07 08:04:38 -0800

    task 2.0

--- nodejs/middleware/verifyToken.js
+++ nodejs/middleware/verifyToken.js
@@ -1,28 +1,17 @@
 const jwt = require('jsonwebtoken');
 
 
-module.exports = function(req,res,next){
-    const token=req.header('auth-token');
-    if (!token) return res.status(401).json({message:"Access denied"});
-    
+module.exports = function (req, res, next) {
+    const token = req.header('auth-token');
+    if (!token) return res.status(401).json({ message: "Access denied" });
 
-    try{
-        const verified=jwt.verify(token,process.env.TOKEN_SECRET);
-        req.user=verified;
+
+    try {
+        const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+        req.user = verified;
         next();
-    }catch(err){
-        res.status(400).json({message:'Invalid Token'});
+    } catch (err) {
+        res.status(400).json({ message: 'Invalid Token' });
 
     }
 }
-
-// router.get('/me', VerifyToken, function(req, res, next) {
-
-//   User.findById(req.userId, { password: 0 }, function (err, user) {
-//     if (err) return res.status(500).send("There was a problem finding the user.");
-//     if (!user) return res.status(404).send("No user found.");
-    
-//     res.status(200).send(user);
-//   });
-  
-// });
